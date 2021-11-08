2c4b4526b03c4ce0c8548121d71f9a907837e755
Thierry Delamare <t.delamare@laposte.net>
2021-11-08 23:20:24 +0100

    First commit

---
+++ fibs.mk
@@ -0,0 +1,25 @@
+#!/usr/bin/make -f
+
+MAKEFLAGS += -Rr --warn-undefined-variables
+SHELL != which bash
+.SHELLFLAGS := -euo pipefail -c
+
+.ONESHELL:
+.DELETE_ON_ERROR:
+.PHONY: phony
+.DEFAULT_GOAL := main
+
+tmp := tmp/fib
+
+hosts := tst2 tst4 tst5 tst6 tst8
+host = $(word $(shell echo $$(($$RANDOM % $(words $(hosts)) + 1))), $(hosts))
+
+fib := fib () { echo "define fib (n) { if (n <= 2) return 1; return fib(n - 1) + fib(n - 2) }; fib($${1:?})" | bc -ql; }
+
+$(tmp)/%: | $(tmp); @ echo -n $* ""; $(fib); { declare -f fib; echo fib $*; } | ssh $(host).admin2 bash > $@; echo -n $$(cat $@) ""
+$(tmp):; mkdir $@
+
+fibs != seq 33
+main: phony $(fibs:%=$(tmp)/%)
+
+clean: phony; rm -r $(tmp)
